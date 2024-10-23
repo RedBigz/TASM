2185d143c5f9636ff10d8e238c9ac5d8ae9f23a3
RedBigz <signoff@redbigz.com>
2024-10-24 07:50:05 +1000

    Add Logging Library similar to Landfall's

---
+++ Logging.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace TASM;
+
+/// <summary>
+/// A log library similar to the one used by TABG's dedicated server
+/// </summary>
+public class Logging
+{
+    public enum LogLevel
+    {
+        Success,
+        Info,
+        Warning,
+        Error,
+    }
+    
+    public static void Log(LogLevel level, string context, string message)
+    {
+        string ansiColor = level switch
+        {
+            LogLevel.Success => "\x1b[32m",
+            LogLevel.Info => "\x1b[34m",
+            LogLevel.Warning => "\x1b[33m",
+            LogLevel.Error => "\x1b[31m",
+            _ => throw new ArgumentOutOfRangeException(nameof(level), level, null)
+        };
+        
+        Console.WriteLine($"\x1b[0m[{ansiColor}{context}\x1b[0m] - \x1b[38;2;128;128;128m{message}\x1b[0m");
+    }
+}
