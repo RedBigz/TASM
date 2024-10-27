92138bac59debfeb3fa1004930f5e63dbb912f42
RedBigz <signoff@redbigz.com>
2024-10-27 11:34:34 +1000

    Make Paths writable only once

--- TASM.Common/Paths.cs
+++ TASM.Common/Paths.cs
@@ -2,7 +2,25 @@ namespace TASM.Common;
 
 public static class Paths
 {
-    public static string? TasmRoot;
-    public static string? TasmPluginsRoot;
-    public static string? TasmDataRoot;
+    private static string? _tasmRoot;
+    private static string? _tasmPluginsRoot;
+    private static string? _tasmDataRoot;
+
+    public static string? TasmRoot
+    {
+        get => _tasmRoot;
+        set => _tasmRoot ??= value;
+    }
+
+    public static string? TasmPluginsRoot
+    {
+        get => _tasmPluginsRoot;
+        set => _tasmPluginsRoot ??= value;
+    }
+
+    public static string? TasmDataRoot
+    {
+        get => _tasmDataRoot;
+        set => _tasmDataRoot ??= value;
+    }
 }
