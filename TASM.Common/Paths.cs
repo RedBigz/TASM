6f1df6b62724bae6330d4256dfddc0da359af438
RedBigz <signoff@redbigz.com>
2024-10-27 11:45:44 +1000

    Comment target folders in Paths.cs

--- TASM.Common/Paths.cs
+++ TASM.Common/Paths.cs
@@ -9,18 +9,21 @@ public static class Paths
     private static string? _tasmPluginsRoot;
     private static string? _tasmDataRoot;
 
+    // ./TASM
     public static string? TasmRoot
     {
         get => _tasmRoot;
         set => _tasmRoot ??= value;
     }
 
+    // ./TASM/Plugins
     public static string? TasmPluginsRoot
     {
         get => _tasmPluginsRoot;
         set => _tasmPluginsRoot ??= value;
     }
 
+    // ./TASM/Data
     public static string? TasmDataRoot
     {
         get => _tasmDataRoot;
