12573b73f86c730e942e30d101487cf147f544c7
RedBigz <signoff@redbigz.com>
2024-10-27 13:24:48 +1000

    Add ExamplePlugin

---
+++ ExamplePlugin/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using Landfall.Network;
+using TASM.Common;
+using Helpers = TASM.Common.Helpers;
+
+[assembly: GamePlugin(name: "Example Plugin", version: "1.0.0", author: "RedBigz")]
+
+namespace ExamplePlugin;
+
+[Command(name: "Echo", reference: "echo", permission: CommandPermission.Everyone)]
+// ReSharper disable once UnusedType.Global
+public static class EchoCommand
+{
+    [CommandEntry]
+    // ReSharper disable once UnusedMember.Global
+    public static void Echo(TABGPlayerServer player, string text)
+    {
+        Helpers.Notification.Notify(player, text);
+    }
+}
